8ecfc3f9b8b58fd17c83b30b1634ad39949daa44
Hung-Ming Wu <whungming@yahoo.com>
2019-07-23 22:53:12 +0800

    add USA part

--- driving.py
+++ driving.py
@@ -6,6 +6,11 @@ if country == 'Taiwan':
 		print('you can take the driving test')
 	else:
 		print('not yet for the driving test')
+elif country == 'USA':
+	if age >= 16:
+		print('you can take the driving test')
+	else:
+		print('not yet for the driving test')
 
 
 
